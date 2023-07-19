2c746e984dfc69634a0cc2f684c9f3dc04ebf757
otfeb <hs970216@naver.com>
2023-07-19 17:09:58 +0900

    day0719

---
+++ javawork/SistJavaProject/src/beakjoon/bj_bronze_1037.java
@@ -0,0 +1,22 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class bj_bronze_1037 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int i,cnt,n;
+		
+		cnt = scanner.nextInt();
+		
+		
+		for(i=0;i<cnt;i++) {
+			n = scanner.nextInt();
+		}
+		
+
+	}
+
+}
---
+++ jswork/htmlwork/js/pop.js
@@ -0,0 +1,7 @@
+function popup1(){
+    window.open('../div_img/beauty1.jpg','n','width=475,height=480');
+}
+
+function naver1(){
+    window.open('https://www.naver.com/','naver','width=1400,height=1400');
+}
